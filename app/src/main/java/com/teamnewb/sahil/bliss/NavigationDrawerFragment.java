792a9ddc77bf8c6091ed8cd3140ab810a89f7ad7
Avinash Thangali <avinashthangali@gmail.com>
2015-02-07 18:58:23 -0800

    HSHacks Test

--- app/src/main/java/com/teamnewb/sahil/bliss/NavigationDrawerFragment.java
+++ app/src/main/java/com/teamnewb/sahil/bliss/NavigationDrawerFragment.java
@@ -102,9 +102,19 @@ public class NavigationDrawerFragment extends Fragment {
                 android.R.layout.simple_list_item_activated_1,
                 android.R.id.text1,
                 new String[]{
-                        getString(R.string.title_section1),
-                        getString(R.string.title_section2),
-                        getString(R.string.title_section3),
+                        "January",
+                        "February",
+                        "March",
+                        "April",
+                        "May",
+                        "June",
+                        "July",
+                        "August",
+                        "September",
+                        "October",
+                        "November",
+                        "December",
+
                 }));
         mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
         return mDrawerListView;
